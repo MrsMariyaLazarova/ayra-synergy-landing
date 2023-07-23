dc996f407b43b90899824ede29b9468d23bf99b3
Mariya Lazarova <67010799+MrsMariyaLazarova@users.noreply.github.com>
2023-07-23 18:36:25 +0300

    Update slideshow.js

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,1 +1,12 @@
-
+$(document).ready(function () {
+  $("#slideshow:visible > div:gt(0)").hide();
+  setInterval(function () {
+    console.log("here");
+    $("#slideshow > div:first")
+      .fadeOut(1000)
+      .next()
+      .fadeIn(1000)
+      .end()
+      .appendTo("#slideshow");
+  }, 3000);
+});
